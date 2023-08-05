248e5b069c7bd228d789d8427288ae9ba534e5a1
Tracy H <aag9n17o@duck.com>
2023-08-05 11:49:34 -0500

    Table is showing up on page, now.

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,16 +2,31 @@ import satData from "./satData";
 
 const satDataList = satData.map(data =>
   <tr key = {data.id}>
-    <td>{satData.name}</td>
-    <td>{satData.type}</td>
-    <td>{satData.launchDate}</td>
-    {/* <td><satStatus /></td> */}
+    <td>{data.name}</td>
+    <td>{data.type}</td>
+    <td>{data.launchDate}</td>
+    {/* <td>{String(data.operational)}</td> */}
+    {/* <td>{
+      if (data.operational === true) {
+        return (
+          Operational
+        );
+      } else {
+        return (
+          Not operational
+        );
+      };
+      }
+    </td> */}
+    {/* <SatStatus /> */}
+    <td>I'm R{data.id}C5</td>
   </tr>
-  );
+);
 
-function satStatus(props) {
-  const status = props.status;
-  if (status) {
+export function SatStatus(props) {
+  const status = props.operational;
+  console.log(status);
+  if (status === true) {
     return (
       <td>Operational</td>
     );
@@ -64,7 +79,28 @@ function satStatus(props) {
 
 const Table = ({sat}) => {
   return (
-     <p>I am a paragraph.</p>>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+          <th>Sample data</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>I'm R0C1</td>
+          <td>I'm R0C2</td>
+          <td>I'm R0C3</td>
+          <td>I'm R0C4</td>
+          <td>I'm R0C5</td>
+        </tr>
+        {satDataList}
+      </tbody>
+    </table>
   );
 };
 
