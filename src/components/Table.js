998604453646c825c7e96393b89f274ff9417ea1
Tracy H <aag9n17o@duck.com>
2023-08-04 19:46:43 -0500

    Working on Table.js.

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,14 +1,35 @@
-const Table = ({ sat }) => {
+import satData from "./satData";
+
+function satStatus(props) {
+  const status = props.status;
+  if (status) {
+    return (
+      <td>Operational</td>
+    );
+  } else {
+    return (
+      <td>Not operational</td>
+    );
+  }
+}
+
+const Table = ({sat}) => {
   return (
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
         </thead>
         <tbody>
         <tr>
-          <td>Row Data TBD</td>
+          <td>{satData.name}</td>
+          <td>{satData.type}</td>
+          <td>{satData.launchDate}</td>
+          <td><satStatus /></td>
         </tr>
         </tbody>
       </table>
@@ -16,3 +37,11 @@ const Table = ({ sat }) => {
 };
 
 export default Table;
+
+// {sat.map((id, data) => {
+//   return (
+//     <button onClick={() => sat(data)} key={id}>
+//       {sat} Orbit
+//     </button>
+//   );
+// })}
